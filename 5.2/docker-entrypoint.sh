d276ffb235f15d6d3e128034ae0800369029d67d
Benjamin <b.wenderoth@gmail.com>
2015-07-18 01:30:00 +0200

    added memsize and creating first collection

--- 5.2/docker-entrypoint.sh
+++ 5.2/docker-entrypoint.sh
@@ -6,6 +6,7 @@ trap 'echo "Shutting down..."; exit 0' SIGINT
 
 if [ -z "$1" ]; then
   exec bin/solr start -f -m $SOLR_MEM_SIZE
+  exec bin/solr create_collection -c collection1 -shards 2
 elif [[ "${1:0:1}" == '-' ]]; then
   exec bin/solr start -f "$@"
   for ((i=10;i--;i>0)); do
