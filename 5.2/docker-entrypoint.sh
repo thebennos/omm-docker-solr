cdd957ea716e438909b844fa5e4c86641c018d83
Benjamin <b.wenderoth@gmail.com>
2015-07-18 17:02:24 +0200

    moved core creation

--- 5.2/docker-entrypoint.sh
+++ 5.2/docker-entrypoint.sh
@@ -5,6 +5,8 @@ trap 'echo "Shutting down..."; exit 0' SIGINT
 
 if [ -z "$1" ]; then
   exec bin/solr start -f -m $SOLR_MEM_SIZE
+  sleep 10
+  exec /opt/solr/bin/solr create -c core1 -d /opt/solr/server/solr/core1
 elif [[ "${1:0:1}" == '-' ]]; then
   exec bin/solr start -f "$@"
   for ((i=10;i--;i>0)); do
